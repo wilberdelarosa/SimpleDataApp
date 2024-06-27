a250629876d828ec0415a857a30f5fa8f355bb1a
WILBER ALFREDO DE LA ROSA MARTE <wd2022-0547@uce.edu.dp>
2024-06-26 20:37:38 -0400

    Agregar archivos de proyecto.

---
+++ SimpleDataApp/Navigation.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace SimpleDataApp
+{
+    partial class Navigation
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGoToAdd = new System.Windows.Forms.Button();
+            this.btnGoToFillOrCancel = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(126, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "iQué desea hacer?";
+            // 
+            // btnGoToAdd
+            // 
+            this.btnGoToAdd.Location = new System.Drawing.Point(89, 112);
+            this.btnGoToAdd.Name = "btnGoToAdd";
+            this.btnGoToAdd.Size = new System.Drawing.Size(286, 35);
+            this.btnGoToAdd.TabIndex = 1;
+            this.btnGoToAdd.Text = "Agregar una cuenta";
+            this.btnGoToAdd.UseVisualStyleBackColor = true;
+            this.btnGoToAdd.Click += new System.EventHandler(this.btnGoToAdd_Click);
+            // 
+            // btnGoToFillOrCancel
+            // 
+            this.btnGoToFillOrCancel.Location = new System.Drawing.Point(40, 189);
+            this.btnGoToFillOrCancel.Name = "btnGoToFillOrCancel";
+            this.btnGoToFillOrCancel.Size = new System.Drawing.Size(393, 35);
+            this.btnGoToFillOrCancel.TabIndex = 2;
+            this.btnGoToFillOrCancel.Text = "Rellenar o cancelar un pedido";
+            this.btnGoToFillOrCancel.UseVisualStyleBackColor = true;
+            this.btnGoToFillOrCancel.Click += new System.EventHandler(this.btnGoToFillOrCancel_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(417, 291);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(112, 35);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Salir";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Navigation
+            // 
+            this.ClientSize = new System.Drawing.Size(562, 357);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnGoToFillOrCancel);
+            this.Controls.Add(this.btnGoToAdd);
+            this.Controls.Add(this.label1);
+            this.Name = "Navigation";
+            this.Load += new System.EventHandler(this.Navigation_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGoToAdd;
+        private System.Windows.Forms.Button btnGoToFillOrCancel;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ SimpleDataApp/NewCustomer.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace SimpleDataApp
+{
+    partial class NewCustomer
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.txtCustomerID = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnCreateAccount = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dtpOrderDate = new System.Windows.Forms.DateTimePicker();
+            this.numOrderAmount = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnAddFinish = new System.Windows.Forms.Button();
+            this.btnAddAnotherAccount = new System.Windows.Forms.Button();
+            this.btnPlaceOrder = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numOrderAmount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre del cliente";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Identificador de Cliente";
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.Location = new System.Drawing.Point(154, 54);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(123, 22);
+            this.txtCustomerName.TabIndex = 3;
+            // 
+            // txtCustomerID
+            // 
+            this.txtCustomerID.Location = new System.Drawing.Point(177, 101);
+            this.txtCustomerID.Name = "txtCustomerID";
+            this.txtCustomerID.Size = new System.Drawing.Size(100, 22);
+            this.txtCustomerID.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtCustomerID);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtCustomerName);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(33, 90);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(334, 195);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Agregar cuerta";
+            // 
+            // btnCreateAccount
+            // 
+            this.btnCreateAccount.Location = new System.Drawing.Point(436, 245);
+            this.btnCreateAccount.Name = "btnCreateAccount";
+            this.btnCreateAccount.Size = new System.Drawing.Size(172, 23);
+            this.btnCreateAccount.TabIndex = 6;
+            this.btnCreateAccount.Text = "Crear cuenta";
+            this.btnCreateAccount.UseVisualStyleBackColor = true;
+            this.btnCreateAccount.Click += new System.EventHandler(this.btnCreateAccount_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dtpOrderDate);
+            this.groupBox2.Controls.Add(this.numOrderAmount);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Location = new System.Drawing.Point(33, 335);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(334, 146);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Crear pedldo";
+            // 
+            // dtpOrderDate
+            // 
+            this.dtpOrderDate.Location = new System.Drawing.Point(147, 107);
+            this.dtpOrderDate.Name = "dtpOrderDate";
+            this.dtpOrderDate.Size = new System.Drawing.Size(181, 22);
+            this.dtpOrderDate.TabIndex = 6;
+            // 
+            // numOrderAmount
+            // 
+            this.numOrderAmount.Location = new System.Drawing.Point(163, 57);
+            this.numOrderAmount.Name = "numOrderAmount";
+            this.numOrderAmount.Size = new System.Drawing.Size(120, 22);
+            this.numOrderAmount.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Cantidad de pedido:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 107);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(113, 16);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Fecha del pedida";
+            // 
+            // btnAddFinish
+            // 
+            this.btnAddFinish.Location = new System.Drawing.Point(64, 505);
+            this.btnAddFinish.Name = "btnAddFinish";
+            this.btnAddFinish.Size = new System.Drawing.Size(126, 23);
+            this.btnAddFinish.TabIndex = 7;
+            this.btnAddFinish.Text = "Finalizar";
+            this.btnAddFinish.UseVisualStyleBackColor = true;
+            this.btnAddFinish.Click += new System.EventHandler(this.btnAddFinish_Click);
+            // 
+            // btnAddAnotherAccount
+            // 
+            this.btnAddAnotherAccount.Location = new System.Drawing.Point(392, 505);
+            this.btnAddAnotherAccount.Name = "btnAddAnotherAccount";
+            this.btnAddAnotherAccount.Size = new System.Drawing.Size(258, 23);
+            this.btnAddAnotherAccount.TabIndex = 8;
+            this.btnAddAnotherAccount.Text = "Agregar Otra cuenta";
+            this.btnAddAnotherAccount.UseVisualStyleBackColor = true;
+            this.btnAddAnotherAccount.Click += new System.EventHandler(this.btnAddAnotherAccount_Click);
+            // 
+            // btnPlaceOrder
+            // 
+            this.btnPlaceOrder.Location = new System.Drawing.Point(392, 458);
+            this.btnPlaceOrder.Name = "btnPlaceOrder";
+            this.btnPlaceOrder.Size = new System.Drawing.Size(181, 23);
+            this.btnPlaceOrder.TabIndex = 9;
+            this.btnPlaceOrder.Text = "Realizar pedido";
+            this.btnPlaceOrder.UseVisualStyleBackColor = true;
+            this.btnPlaceOrder.Click += new System.EventHandler(this.btnPlaceOrder_Click);
+            // 
+            // NewCustomer
+            // 
+            this.ClientSize = new System.Drawing.Size(662, 558);
+            this.Controls.Add(this.btnPlaceOrder);
+            this.Controls.Add(this.btnAddAnotherAccount);
+            this.Controls.Add(this.btnAddFinish);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnCreateAccount);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "NewCustomer";
+            this.Load += new System.EventHandler(this.Navigation_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numOrderAmount)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.TextBox txtCustomerID;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnCreateAccount;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnAddFinish;
+        private System.Windows.Forms.Button btnAddAnotherAccount;
+        private System.Windows.Forms.Button btnPlaceOrder;
+        private System.Windows.Forms.DateTimePicker dtpOrderDate;
+        private System.Windows.Forms.NumericUpDown numOrderAmount;
+    }
+}
+
